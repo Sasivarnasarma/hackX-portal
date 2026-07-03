b0231af1c0d815793749379e6186c955ebf65605
Sasivarnasarma <sasivarnasarma@gmail.com>
2026-07-03 21:00:33 +0530

    bug fix

--- frontend-user/src/pages/RegisterJr.tsx
+++ frontend-user/src/pages/RegisterJr.tsx
@@ -49,16 +49,6 @@ const JR_SOURCE_OPTIONS = [
   { value: 'Other', label: 'Other' }
 ];
 
-const calculateAge = (birthDateString: string): number => {
-  const today = new Date();
-  const birthDate = new Date(birthDateString);
-  let age = today.getFullYear() - birthDate.getFullYear();
-  const m = today.getMonth() - birthDate.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-      age--;
-  }
-  return age;
-};
 
 const RegisterJr: React.FC = () => {
   useEffect(() => {
